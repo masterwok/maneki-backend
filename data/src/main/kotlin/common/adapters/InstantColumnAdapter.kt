7ac41f54b90f1259b3a9e701f4986e650e57e8b3
Jonathan Trowbridge <jonathan.trowbridge@coinme.com>
2023-04-03 01:49:33 -0400

    Support fractional seconds

--- data/src/main/kotlin/common/adapters/InstantColumnAdapter.kt
+++ data/src/main/kotlin/common/adapters/InstantColumnAdapter.kt
@@ -6,11 +6,13 @@ import kotlinx.datetime.toJavaInstant
 import kotlinx.datetime.toKotlinInstant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
 
 
 object TimestampColumnAdapter : ColumnAdapter<Instant, String> {
     private val dateTimeFormat = DateTimeFormatterBuilder()
         .appendPattern("yyyy-MM-dd HH:mm:ss")
+        .appendFraction(ChronoField.MILLI_OF_SECOND, 0, 3, true)
         .toFormatter()
         .withZone(ZoneId.of("UTC"))
 
