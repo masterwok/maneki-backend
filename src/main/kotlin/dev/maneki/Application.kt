fcedfd6f91b143191d7779cac0f6adafd0687066
Jonathan Trowbridge <jonathan.trowbridge@coinme.com>
2023-06-19 17:24:27 -0400

    WIP on content negotiation
    Swagger is being generated without the correct content type and requests are always failing

--- src/main/kotlin/dev/maneki/Application.kt
+++ src/main/kotlin/dev/maneki/Application.kt
@@ -4,24 +4,23 @@ import dev.maneki.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.openapi.*
-import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(
         Netty,
         port = 80,
         host = "0.0.0.0",
-        module = Application::module
+        module = Application::module,
     ).start(wait = true)
+
 }
 
 fun Application.module() {
+    configureCORS()
     configureKoin()
     configureSecurity()
     configureMonitoring()
-    configureSerialization()
+    configureContentNegotiation()
     configureRouting()
-    configureCORS()
 }
 
